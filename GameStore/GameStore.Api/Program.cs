2e0f0b496f5f771f7adefa89333b65867772e5a6
mithatsarikaya <mithatsarikaya58@gmail.com>
2024-04-29 22:05:05 +0300

    all CRUD with local list

--- GameStore/GameStore.Api/Program.cs
+++ GameStore/GameStore.Api/Program.cs
@@ -20,7 +20,7 @@ app.MapGet("games/{id}", (int id) => games.Find(x => x.Id == id))
     .WithName(GetGameEndpointName);
 
 
-// POST /games
+// POST /games  
 app.MapPost("games", (CreateGameDto newGame) =>
 {
     GameDto game = new(
@@ -35,5 +35,33 @@ app.MapPost("games", (CreateGameDto newGame) =>
 
     return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
 });
+
+// PUT /games  
+app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
+{
+
+    var index = games.FindIndex(game => game.Id == id);
+
+    games[index] = new GameDto(
+        id,
+        updatedGame.Name,
+        updatedGame.Genre,
+        updatedGame.Price,
+        updatedGame.ReleaseDate
+
+    );
+
+    return Results.NoContent();
+});
+
+
+// DELETE /games/1
+app.MapDelete("games/{id}", (int id) =>
+{
+    games.RemoveAll(game => game.Id == id);
+
+    return Results.NoContent();
+});
+
 
 app.Run();
